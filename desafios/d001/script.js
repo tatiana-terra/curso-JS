b8338d2e75f7d2bd02b4f6cb197eb3a63e577f04
Tatiana Terra <tatianamarsicano@gmail.com>
2025-04-11 18:51:30 -0300

    Desafios

---
+++ desafios/d001/script.js
@@ -0,0 +1,12 @@
+
+function clicar1(){
+    window.alert('Você clicou no primeiro botão')
+}
+
+function clicar2(){
+    window.alert('Você clicou no segundo botão')
+}
+
+function clicar3(){
+    window.alert('Você clicou no terceiro botão')
+}
