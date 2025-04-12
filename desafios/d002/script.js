a3d80e314ec1c18b37de9bf130a7bd2b8193654b
Tatiana Terra <tatianamarsicano@gmail.com>
2025-04-12 09:46:13 -0300

    Desafio 002

---
+++ desafios/d002/script.js
@@ -0,0 +1,6 @@
+function clicar() {
+    let nome = window.prompt('Qual é o seu nome?')
+    let idade = window.prompt('Qual a sua idade?')
+    window.alert(`Acabei de conhecer ${nome}, que tem ${idade} anos de idade!`)
+}
+
